bc43d23a5c838410fd5d29b4508f16cc4a55b5b5
Jared Fields <jbfields@protonmail.com>
2019-03-08 15:54:20 -0600

    Adds documentation which will show up in Python with help()
    Removes print_usage() in favor of in-function documentation
    which shows up when we use help(nimformat).
    
    Moves the example into the comment under nim_sub()

--- nimformat.nim
+++ nimformat.nim
@@ -1,7 +1,42 @@
 import nimpy, tables, pylib, re
 
+#[
+  Build with:
+    nim c --threads:on --app:lib --out:nimformat.pyd --tlsEmulation:off nimformat.nim
+]#
 
 proc nim_sub*(s: string, dict: Table[string, string]): string {.exportpy.} =
+  ## nim_sub(string, dict)
+  ## 
+  ## Receives a string in which all variables to be substituted are in the format 
+  ## {variable}, as well as a dictionary in {'variable': 'value'} format.
+  ## For each variable in the dictionary the function substitutes the value for the 
+  ## variable name.
+  ##
+  ## Returns the supplied string with all substitutions applied.
+  ##
+  ## Example
+  ## =======
+  ## import nimformat
+  ##
+  ## strWithSubstitutions = '''
+  ## This is a {variable1} which spans
+  ## multiple lines and {variable2} multiple
+  ## substitutions.
+  ## '''
+  ##
+  ## variable1 = 'string'
+  ## variable2 = 'contains'
+  ##
+  ## nimFormatEnv = dict(locals())
+  ## nimFormatEnv = {str(key): str(nimFormatEnv[key]) for key in nimFormatEnv.keys() if '__' not in str(key)}
+  ##
+  ## strSubstituted = nimformat.nim_sub(strWithSubstitutions, nimFormatEnv)
+  ## print(strSubstituted)
+  ## 
+  ## This is a string which spans
+  ## multiple lines and contains multiple
+  ## substitutions.
   var s = s
   for key, val in dict:
     s = (s.replace(re("({" & key & "})"), val))
@@ -10,6 +45,7 @@ proc nim_sub*(s: string, dict: Table[string, string]): string {.exportpy.} =
 
 
 proc test_nim_sub*() {.exportpy.} =
+  ## Tests the functionality of nim_sub()
   var
     # table_sub = initTable[string, string]()
     table_sub = {"": ""}.toTable
@@ -28,26 +64,3 @@ proc test_nim_sub*() {.exportpy.} =
 
   let result = nim_sub(strTest, table_sub)
   echo(result)
-
-
-proc print_usage() {.exportpy.} =
-  echo("""
-  import nimformat
-
-  strWithSubstitutions = '''
-  This is a {variable1} which spans
-  multiple lines and {variable2} multiple
-  substitutions.
-  '''
-
-  variable1 = 'string'
-  variable2 = 'contains'
-
-  nimFormatEnv = dict(locals())
-  nimFormatEnv = {str(key): str(nimFormatEnv[key]) for key in nimFormatEnv.keys() if '__' not in str(key)}
-
-  strSubstituted = nimformat.nim_sub(strWithSubstitutions, nimFormatEnv)
-  print(strSubstituted)
-  """)
-
-  test_nim_sub()
